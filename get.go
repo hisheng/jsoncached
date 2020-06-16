85eabff4a605bc907a22ef475c5e6cd136e4ccb1
张海生 <zhanghaisheng@km.com>
2020-06-16 17:00:26 +0800

    增加error

--- get.go
+++ get.go
@@ -1,7 +1,13 @@
 package jsoncached
 
-func Get(key string) []byte {
-	return getFile(key)
+import "errors"
+
+func Get(key string) ([]byte, error) {
+	rs := getFile(key)
+	if len(rs) == 0 {
+		return []byte{}, errors.New("没有文件")
+	}
+	return rs, nil
 }
 
 func GetString(key string) string {
